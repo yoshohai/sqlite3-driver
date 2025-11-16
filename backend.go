15e738cc665d6bbbe827cc52f52452e493c0b669
Hai Hai <134259902+yoshohai@users.noreply.github.com>
2025-11-16 22:59:37 +0530

    flags

--- backend.go
+++ backend.go
@@ -3,22 +3,28 @@ package driver
 import "unsafe"
 
 type SQLiteBackend interface {
-	CharPtr(p unsafe.Pointer) unsafe.Pointer
-	StringData(s string) unsafe.Pointer
+	OpenReadWrite() int
+	OpenReadOnly() int
+	OpenCreate() int
+	OpenMemory() int
+	OpenNoMutex() int
+	OpenFullMutex() int
+	OpenURI() int
+	OpenExtendedResultCode() int
 
 	OpenV2(filename unsafe.Pointer, ppDb unsafe.Pointer, flags int, zVfs unsafe.Pointer) int
 	CloseV2(db unsafe.Pointer) int
 	Exec(db unsafe.Pointer, sql unsafe.Pointer) int
-	ExtendedResultCodes(db unsafe.Pointer, onoff int) int
 
 	PrepareV2(db unsafe.Pointer, zSql unsafe.Pointer, ppStmt unsafe.Pointer) int
+	Step(stmt unsafe.Pointer) int
+	Reset(stmt unsafe.Pointer) int
+	Finalize(stmt unsafe.Pointer) int
+
 	BindInt64(stmt unsafe.Pointer, index int, value int64) int
 	BindDouble(stmt unsafe.Pointer, index int, value float64) int
 	BindText(stmt unsafe.Pointer, index int, value unsafe.Pointer, n int) int
 	BindNull(stmt unsafe.Pointer, index int) int
-	Step(stmt unsafe.Pointer) int
-	Reset(stmt unsafe.Pointer) int
-	Finalize(stmt unsafe.Pointer) int
 
 	ColumnCount(stmt unsafe.Pointer) int
 	ColumnName(stmt unsafe.Pointer, i int) string
@@ -31,12 +37,11 @@ type SQLiteBackend interface {
 	ErrMsg(db unsafe.Pointer) string
 	ErrStr(rc int) string
 
+	CharPtr(p unsafe.Pointer) unsafe.Pointer
+	StringData(s string) unsafe.Pointer
+
 	ResultOk() int
 	ResultRow() int
 	ResultDone() int
 	ResultNull() int
-	OpenReadWrite() int
-	OpenCreate() int
-	OpenNoMutex() int
-	OpenURI() int
 }
